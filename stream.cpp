6f5d22c2eb67b3f8c163770f71b58270d5d98214
Bernhard Rosenkränzer <bero@lindev.ch>
2017-08-11 17:50:54 +0200

    Allow switching channels

--- stream.cpp
+++ stream.cpp
@@ -17,12 +17,15 @@ extern "C" {
 
 std::mutex newClientsMutex;
 std::vector<int> newClients;
+std::mutex newChannelMutex;
+auto newChannel = std::make_pair<Transponder const *,Service const *>(nullptr, nullptr);
 
 void httpThread(int fd) {
 	pollfd p;
 	p.fd = fd;
 	p.events = POLLIN|POLLHUP;
 	bool isGet = false, isHead = false, isBad = false, complete = false;
+	std::string path;
 	while(!complete) {
 		char buf[1024];
 		if(poll(&p, 1, 10000)>0) {
@@ -43,11 +46,33 @@ void httpThread(int fd) {
 			char *line = strtok_r(buf, "\n", &tmp);
 			while(line) {
 				Util::hexdump(reinterpret_cast<unsigned char*>(line), strlen(line));
-				if(!strncasecmp(line, "HEAD ", 5))
-					isHead = true;
-				else if(!strncasecmp(line, "GET ", 4))
-					isGet = true;
-				else if(!isHead && !isGet) {
+				bool curHead = !strncasecmp(line, "HEAD ", 5);
+				bool curGet = !strncasecmp(line, "GET ", 4);
+				if(curHead || curGet) {
+					isHead = curHead;
+					isGet = curGet;
+					char *tmp1;
+					char *s = strtok_r(line, " ", &tmp1);
+					// Skip over "HEAD /" or "GET /" -- we've already seen that
+					s = strtok_r(nullptr, " ", &tmp1);
+					for(int i=1; i<strlen(s); i++) {
+						if(i<strlen(s)-2 && s[i] == '%' &&
+								((s[i+1] >= '0' && s[i+1] <= '9') || (s[i+1] >= 'a' && s[i+1] <= 'f') || (s[i+1] >= 'A' && s[i+1] <= 'F')) &&
+								((s[i+2] >= '0' && s[i+2] <= '9') || (s[i+2] >= 'a' && s[i+2] <= 'f') || (s[i+2] >= 'A' && s[i+2] <= 'F'))) {
+							char code[3] = {s[i+1], s[i+2], 0};
+							path.push_back(strtoul(code, nullptr, 16));
+							i+=2;
+						} else if(s[i] == '+')
+							path.push_back(' ');
+						else
+							path.push_back(s[i]);
+					}
+					s=strtok_r(nullptr, "", &tmp1);
+					if(!s || !strncmp(s, "HTTP/0", 6)) { // HTTP before 1.0 -- no extras expected
+						complete = true;
+						break;
+					}
+				} else if(!isHead && !isGet) {
 					isBad = true; complete = true;
 					break;
 				}
@@ -59,15 +84,28 @@ void httpThread(int fd) {
 		}
 	}
 	if(complete) {
+		std::cerr << "Got request for " << path << std::endl;
+		ChannelList channels("channels.dvb");
+		auto channel = channels.find(path);
+		if(!channel.first)
+			std::cerr << "No such channel!" << std::endl;
 		if(isGet || isHead) {
 			send(fd, "HTTP/1.1 200 OK\r\n", 17, 0);
 			send(fd, "Cache-Control: no-store, no-cache\r\n", 35, 0);
 			send(fd, "Content-Type: video/mp2t\r\n", 26, 0);
 			send(fd, "\r\n", 2, 0);
 			if(isGet) {
+				// FIXME don't allow switching the channel if we're already
+				// streaming to someone else...
+				{
+					std::lock_guard<std::mutex> channelGuard(newChannelMutex);
+					newChannel = channel;
+				}
 				// Start sending the stream...
-				std::lock_guard<std::mutex> guard(newClientsMutex);
-				newClients.push_back(fd);
+				{
+					std::lock_guard<std::mutex> guard(newClientsMutex);
+					newClients.push_back(fd);
+				}
 				std::cerr << "Client added" << std::endl;
 			} else {
 				close(fd);
@@ -159,18 +197,33 @@ int main(int argc, char **argv) {
 				int conn = accept(s, &dest, &ss);
 				std::thread *http = new std::thread(&httpThread, conn);
 			}
+			{
+				std::lock_guard<std::mutex> channelGuard(newChannelMutex);
+				if(newChannel.first) {
+					close(dvbFd);
+					cards[0].tune(newChannel.first);
+					cards[0].setup(*newChannel.second);
+					newChannel=std::make_pair<Transponder const *, Service const *>(nullptr, nullptr);
+					dvbFd = open("/dev/dvb/adapter0/dvr0", O_RDONLY|O_NONBLOCK);
+					p[1].fd = dvbFd;
+					continue;
+				}
+			}
 			if(p[1].revents & POLLIN) {
 				size_t bytes = read(dvbFd, dvbbuf, 188);
+				if(bytes>0)
 				{
-					std::lock_guard<std::mutex> guard(newClientsMutex);
-					for(int fd: newClients) {
-						p[nfds].fd = fd;
-						p[nfds++].events = POLLOUT|POLLHUP;
+					{
+						std::lock_guard<std::mutex> guard(newClientsMutex);
+						for(int fd: newClients) {
+							p[nfds].fd = fd;
+							p[nfds++].events = POLLOUT|POLLHUP;
+						}
+						newClients=std::vector<int>();
 					}
-					newClients=std::vector<int>();
+					for(int i=2; i<nfds; i++)
+						write(p[i].fd, dvbbuf, bytes);
 				}
-				for(int i=2; i<nfds; i++)
-					write(p[i].fd, dvbbuf, bytes);
 			}
 		}
 	}
